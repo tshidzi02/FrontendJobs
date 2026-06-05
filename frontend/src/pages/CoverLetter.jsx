4ff428a02150a4b98ace804b554c18f6d5e9ec59
Mutshidzi Madzivhandila <tshidzi02@gmail.com>
2026-06-05 19:25:23 +0200

    coverletter

--- frontend/src/pages/CoverLetter.jsx
+++ frontend/src/pages/CoverLetter.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import api from "../services/api";
 import DashboardLayout from "../layouts/DashboardLayout";
 
